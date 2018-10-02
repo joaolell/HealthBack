61767d6a8b835b9cb2893ed56be279427b7522c0
Rafael Toshio <rafaeluemura18@gmail.com>
2018-10-02 20:15:15 -0300

    Metodos para gerar filtros

--- HealthControlAPI/Controllers/FiltersController.cs
+++ HealthControlAPI/Controllers/FiltersController.cs
@@ -44,6 +44,98 @@ namespace HealthControlAPI.Controllers
             return JsonConvert.SerializeObject(retorno);
         }
 
+        // GET: api/Users
+        [Route("all2")]
+        [HttpGet]
+        public List<ResultSetMySql> getTop10Fabricantes()
+        {
+
+            string query = "SELECT NOME_PAIS_FABRIC, COUNT(NOME_PAIS_FABRIC) FROM anvisa_dados " +
+                           " GROUP BY NOME_PAIS_FABRIC ORDER BY COUNT(NOME_PAIS_FABRIC) DESC limit 10 ";
+            string config = "server=;username=;password=;database=;SslMode=none";
+
+            MySqlConnection connection = new MySqlConnection(config);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataReader Reader = command.ExecuteReader();
+
+            List<ResultSetMySql> lista = new List<ResultSetMySql>();
+
+            while (Reader.Read())
+            {
+                ResultSetMySql obj = new ResultSetMySql();
+                obj.Ret1 = Reader[0].ToString();
+                obj.Ret2 = Reader[1].ToString();
+
+                lista.Add(obj);
+
+            }
+            connection.Close();
+            return lista;
+        }
+
+
+        // GET: api/Users
+        [Route("all2")]
+        [HttpGet]
+        public List<ResultSetMySql> getCountLaboratorios()
+        {
+
+            string query = "SELECT DETENTOR_REGISTRO_CADASTRO, COUNT(DETENTOR_REGISTRO_CADASTRO)" +
+                           "FROM ANVISA_DADOS  GROUP BY DETENTOR_REGISTRO_CADASTRO ORDER BY COUNT(DETENTOR_REGISTRO_CADASTRO)DESC limit 10";
+            string config = "server=;username=;password=;database=;SslMode=none";
+
+            MySqlConnection connection = new MySqlConnection(config);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataReader Reader = command.ExecuteReader();
+
+            List<ResultSetMySql> lista = new List<ResultSetMySql>();
+
+            while (Reader.Read())
+            {
+                ResultSetMySql obj = new ResultSetMySql();
+                obj.Ret1 = Reader[0].ToString();
+                obj.Ret2 = Reader[1].ToString();
+
+                lista.Add(obj);
+
+            }
+            connection.Close();
+            return lista;
+        }
+
+        // GET: api/Users
+        [Route("all2")]
+        [HttpGet]
+        public List<ResultSetMySql> getRiscosPorVigencia()
+        {
+
+            string query = " SELECT VALIDADE_REGISTRO_CADASTRO, CLASSE_RISCO, COUNT(CLASSE_RISCO)" +
+                           "FROM ANVISA_DADOS WHERE VALIDADE_REGISTRO_CADASTRO= 'VIGENTE' GROUP BY CLASSE_RISCO" +
+                           "ORDER BY COUNT(CLASSE_RISCO)DESC  ";
+            string config = "server=;username=;password=;database=;SslMode=none";
+
+            MySqlConnection connection = new MySqlConnection(config);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            connection.Open();
+            MySqlDataReader Reader = command.ExecuteReader();
+
+            List<ResultSetMySql> lista = new List<ResultSetMySql>();
+
+            while (Reader.Read())
+            {
+                ResultSetMySql obj = new ResultSetMySql();
+                obj.Ret1 = Reader[0].ToString();
+                obj.Ret2 = Reader[1].ToString();
+
+                lista.Add(obj);
+
+            }
+            connection.Close();
+            return lista;
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
---
+++ HealthControlAPI/Models/ResultSetMySql.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthControlAPI.Models
+{
+    public class ResultSetMySql
+    {
+        public string Ret1 { get; set; }
+        public string Ret2 { get; set; }
+        public string Ret3 { get; set; }
+        public string Ret4 { get; set; }
+    }
+}
